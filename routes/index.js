c8d1c992bea47abaada62e4b63a0456d2e4bf170
eugenehiggins <genotype@gknow.com>
2016-03-17 11:33:14 -0600

    added ability to enter natural date at paramter for root route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var outputDate = function(date) {
+    var month = date.toLocaleString("en-us", { month: "long"});
+    var day = date.getDate();
+    var year = date.getFullYear()
+
+    return JSON.stringify({ "unix": date.getTime()/1000 , "natural": month +" "+ day +" "+ year })
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,20 +16,21 @@ router.get('/', function(req, res, next) {
 
 router.route('/:date')
     .get(function (req, res) {
+        var param = req.params.date;
 
-      var param = req.params.date;
+        var date;
 
-      if(!isNaN(param)){
-        var unixtimeInt = Number.parseInt(param);
-        var date = new Date(unixtimeInt * 1000);
-        var month = date.toLocaleString("en-us", { month: "long"});
-        var day = date.getDate();
-        var year = date.getFullYear()
+        if(!isNaN(param)){
+            var unixtimeInt = Number.parseInt(param);
+            date = new Date(unixtimeInt * 1000);
 
-        res.send(JSON.stringify({ "unix": unixtimeInt , "natural": month +" "+ day +" "+ year }));
-      } else {
+        } else {
+            var dateStr = param;
+            date = new Date(param);
+            //console.log(date);
+        }
 
-      }
+        res.send(outputDate(date));
     });
 
 
